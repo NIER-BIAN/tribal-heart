ac684d83ff3b741bc585ced63b995e667bfc9847
NIER_BIAN <58038514+CoolGuySergey@users.noreply.github.com>
2024-10-07 23:15:30 +0800

    added loader

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import { getEvents, extractLocations } from './api';
 
 import './App.css';
 
+import { cardio } from 'ldrs'
+
+cardio.register()
+
 const App = () => {
 
     //========================================================================================
@@ -20,8 +24,13 @@ const App = () => {
     const [allLocations, setAllLocations] = useState([]);
     const [numberPerPage, setNumberPerPage] = useState(10);
     const [currentCity, setCurrentCity] = useState("See all cities");
+    const [isLoading, setIsLoading] = useState(true);
     
     const fetchData = async () => {
+
+	// start loader
+	setIsLoading(true);
+	
 	const eventsData = await getEvents();
 	setAllLocations(extractLocations(eventsData));
 	
@@ -33,6 +42,9 @@ const App = () => {
 	// For now, replace eventsData with filteredEvents
 	// setAllEvents(eventsData.slice(0, numberPerPage));
 	setAllEvents(filteredEvents.slice(0, numberPerPage))
+
+	// stop loader
+	setIsLoading(false);
     }
 
     //=========================================================================================
@@ -52,25 +64,40 @@ const App = () => {
     // UI RENDERING
     
     return (
-	    <div className="App">
 	    
+	    <div className="App">
+
 	      <CitySearch
 	         allLocations={allLocations}
 	         setCurrentCity={setCurrentCity}
 	      />
 
-	    <div className="pagination">
-	      <p>Items per page:</p>
-	      <NumberOfEvents
-	        numberPerPage={numberPerPage}
-	        onSelectionClick={(newNumberPerPage) => setNumberPerPage(newNumberPerPage)}
+	      <div className="pagination">
+	        <p>Items per page:</p>
+	        <NumberOfEvents
+	          numberPerPage={numberPerPage}
+	          onSelectionClick={(newNumberPerPage) => setNumberPerPage(newNumberPerPage)}
 	      />
-	    </div>
-	    
-	    <EventList
-	        allEvents={allEvents}
-	    />
-	    
+	      </div>
+
+	      {isLoading && (
+	        <div className="loading-message">
+	          <h2>Loading...</h2>
+	          <l-cardio
+	            size="120"
+	            stroke="4"
+	            speed="0.9" 
+	            color="white" 
+	          ></l-cardio>
+	        </div>
+	      )}
+
+	      {!isLoading && (
+	        <EventList
+	          allEvents={allEvents}
+	        />
+	      )}
+	
 	</div>
     );
 }
